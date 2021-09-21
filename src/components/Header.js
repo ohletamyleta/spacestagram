6d38169896f69a6c8a3e6c2852f3660f32d9778c
ohletamyleta <ohletamyleta@gmail.com>
2021-09-21 13:41:50 -0400

    build header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+    <div className="heading">
+ <h1>Welcome To Spacestagram!</h1>
+ <h3><em>Today's selection of random photos from across the universe</em></h3>
+</div>
+  );
+};
+
+export default Header;
